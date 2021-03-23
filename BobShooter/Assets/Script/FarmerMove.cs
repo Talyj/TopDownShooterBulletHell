b21cbe0912ead34890ca855e45e0d35668d7eacf
sepiiroth <37743946+sepiiroth@users.noreply.github.com>
2021-03-23 11:51:22 +0100

    Farmer Move

--- BobShooter/Assets/Script/FarmerMove.cs
+++ BobShooter/Assets/Script/FarmerMove.cs
@@ -7,27 +7,47 @@ public class FarmerMove : MonoBehaviour
     List<ScriptableObject> Modules;
     public MoveModule moveComponent;
 
-    public Vector2 speed = new Vector2(10, 10);
+    private Vector2 speed = new Vector2(0.1F, 0.1F);
+    
+    private float frequency;
+    private float magnitude;
 
-    private void Start()
-    {
+    [SerializeField]
+    public GameObject farmer;
 
+    Vector3 pos, localScale, move;
 
+    private void Start() {
         //moveComponent = new MoveModule(speed.x * Input.GetAxis("Horizontal"), speed.y * Input.GetAxis("Vertical"), this.gameObject.transform);
         moveComponent = new MoveModule(speed.x , speed.y, this.gameObject.transform);
 
         Modules = new List<ScriptableObject>();
         Modules.Add(moveComponent);
         TAccessor<MoveModule>.Instance().Add(moveComponent);
+
+        frequency = 2.0F;
+        magnitude = Random.Range(0.005F, 0.008F);
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        float inputX = Input.GetAxis("Horizontal");
-        float inputY = Input.GetAxis("Vertical");
+        move = new Vector3(speed.x, speed.y, 0) * Time.deltaTime;
+        
+        if(farmer.tag == "FarmerHorizontal") {
+            MoveHorizontal();
+        } else if(farmer.tag == "FarmerVertical") {
+            MoveVertical();
+        }
+    }
 
-        Vector3 move = new Vector3(speed.x , speed.y, 0) * Time.deltaTime;
+    private void MoveHorizontal() {
+        transform.position += move + transform.up * Mathf.Sin(Time.time * frequency) * magnitude;
+        transform.position += transform.right * Time.deltaTime;
+    }
 
-        transform.Translate(move);
+    private void MoveVertical() {
+        transform.position += move + transform.right * Mathf.Sin(Time.time * frequency) * magnitude;
+        transform.position -= transform.up * Time.deltaTime;
     }
 }
+ 
